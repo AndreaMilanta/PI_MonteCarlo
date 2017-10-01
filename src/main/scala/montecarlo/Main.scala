3ade8dcceb8bde0afdfdf80e64b6d0373118a037
Andrea Milanta <AndreaMilanta@users.noreply.github.com>
2017-09-30 19:49:07 -0500

    Optimized for reduced memory usage

--- src/main/scala/montecarlo/Main.scala
+++ src/main/scala/montecarlo/Main.scala
@@ -11,11 +11,22 @@ import scala.util.Random
 
 object Main extends App{
 
-  val NUM_TRIES = 1000000     //number of random throws
-  def randArray = (1 to NUM_TRIES).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
-  val randResults = randArray.map(_.inCircle())
-  val circleArea = randResults.count(_ == true)
-  def pi:Double =  4 * circleArea.toFloat / NUM_TRIES
+  //To avoid memory overflow the computation is split in multiple steps
+  val NUM_TRIES = 1000000000     //number of random throws
+  val MAX_ARRAY_SIZE = 1000000
+  var circleArea: Int = 0
+
+  for(i <- 1 to NUM_TRIES by MAX_ARRAY_SIZE) {
+    var randArray =
+      if ((i + MAX_ARRAY_SIZE - 1) > NUM_TRIES)
+        List.fill(NUM_TRIES - i)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
+      else
+        List.fill(MAX_ARRAY_SIZE)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
+
+    circleArea += randArray.count(_.inCircle())
+  }
+
+  val pi:Double =  4 * circleArea.toFloat / NUM_TRIES
 
   println(f"pi = 4 * $circleArea / $NUM_TRIES = $pi%.5f ")
 
