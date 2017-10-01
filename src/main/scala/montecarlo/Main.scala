df0587a00a62f45b3f14341a42588b1e8f767c5b
AndreaMilanta <andrea.milanta@gmail.com>
2017-09-30 20:21:59 -0500

    Slight optimization on List initialization

--- src/main/scala/montecarlo/Main.scala
+++ src/main/scala/montecarlo/Main.scala
@@ -19,15 +19,17 @@ object Main extends App{
   for(i <- 1 to NUM_TRIES by MAX_ARRAY_SIZE) {
     var randArray =
       if ((i + MAX_ARRAY_SIZE - 1) > NUM_TRIES)
-        List.fill(NUM_TRIES - i)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
+        //List.fill(NUM_TRIES - i)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
+        List.fill(NUM_TRIES - i)(new Point(Random.nextDouble(), Random.nextDouble()))
       else
-        List.fill(MAX_ARRAY_SIZE)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
+        //List.fill(MAX_ARRAY_SIZE)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
+        List.fill(MAX_ARRAY_SIZE)(new Point(Random.nextDouble(), Random.nextDouble()))
 
     circleArea += randArray.count(_.inCircle())
   }
 
   val pi:Double =  4 * circleArea.toFloat / NUM_TRIES
 
-  println(f"pi = 4 * $circleArea / $NUM_TRIES = $pi%.5f ")
+  println(f"pi = 4 * $circleArea / $NUM_TRIES = $pi%.8f ")
 
 }
