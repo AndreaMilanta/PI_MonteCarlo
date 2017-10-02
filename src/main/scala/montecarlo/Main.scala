1645de6e102d4744b4b0fc5403a4b7d76ef3623f
Andrea Milanta <AndreaMilanta@users.noreply.github.com>
2017-10-01 20:01:55 -0500

    Using normal distribution from R Very slow!

--- src/main/scala/montecarlo/Main.scala
+++ src/main/scala/montecarlo/Main.scala
@@ -5,14 +5,15 @@
 
 package montecarlo
 
-import PI._
-
 import scala.util.Random
+import PI._
 
 object Main extends App{
 
+  val R = org.ddahl.rscala.RClient()
+
   //To avoid memory overflow the computation is split in multiple steps
-  val NUM_TRIES = 1000000000     //number of random throws
+  val NUM_TRIES = 1000     //number of random throws
   val MAX_ARRAY_SIZE = 1000000
   var circleArea: Int = 0
 
@@ -20,16 +21,24 @@ object Main extends App{
     var randArray =
       if ((i + MAX_ARRAY_SIZE - 1) > NUM_TRIES)
         //List.fill(NUM_TRIES - i)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
-        List.fill(NUM_TRIES - i)(new Point(Random.nextDouble(), Random.nextDouble()))
+        //List.fill(NUM_TRIES - i)(new Point(Random.nextDouble(), Random.nextDouble()))
+        List.fill(NUM_TRIES - i)(new Point(normRand(), normRand()))
       else
         //List.fill(MAX_ARRAY_SIZE)(null).map(_ => new Point(Random.nextDouble(), Random.nextDouble()))
-        List.fill(MAX_ARRAY_SIZE)(new Point(Random.nextDouble(), Random.nextDouble()))
+        //List.fill(MAX_ARRAY_SIZE)(new Point(Random.nextDouble(), Random.nextDouble()))
+        List.fill(MAX_ARRAY_SIZE)(new Point(normRand(), normRand()))
 
     circleArea += randArray.count(_.inCircle())
   }
 
   val pi:Double =  4 * circleArea.toFloat / NUM_TRIES
-
   println(f"pi = 4 * $circleArea / $NUM_TRIES = $pi%.8f ")
 
+  def normRand(): Double = {
+    var c:Double = - 1
+    while(c < 0 || c > 1 )
+      c = R.evalD0("rnorm(1,0.5,0.2)")
+    c
+  }
 }
+
